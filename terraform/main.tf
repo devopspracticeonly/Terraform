6641295dbaf34020a42b379c617514fafdeb3b07
Your Name <you@example.com>
2026-09-14 12:56:04 +0530

    Jenkinsfile added to it

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ region = "us-east-1"
 
 resource "aws_instance" "web" {
 ami           = "ami-00244c37cc386a210"
-instance_type = "t2.micro"
+instance_type = var.instance_type
 
 tags = {
 Name = "terraform-web"
