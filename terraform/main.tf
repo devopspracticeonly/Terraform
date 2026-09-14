b586abb662cbb14268e9a113090fe8e09b78791c
Your Name <you@example.com>
2026-09-14 12:22:43 +0530

    Jenkinsfile added to it

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+required_version = ">= 1.6.0"
+}
+
+
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+ami           = "ami-00244c37cc386a210"
+instance_type = "t2.micro"
+
+tags = {
+Name = "terraform-web"
+}
+}
